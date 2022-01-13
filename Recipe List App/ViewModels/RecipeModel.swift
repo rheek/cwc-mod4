928be5bcb0653758cef6788fff04e354ca4916ab
Joshua Goble <gobleanimation@gmail.com>
2022-01-12 23:47:12 -0700

    Calculate and display portions in RecipeModel Display portions in DetailView

--- Recipe List App/ViewModels/RecipeModel.swift
+++ Recipe List App/ViewModels/RecipeModel.swift
@@ -19,5 +19,83 @@ class RecipeModel: ObservableObject {
         
     }
     
+    static func getPortion(ingredient:Ingredient, recipeServings:Int, targetServings:Int) -> String {
+        
+        var portion = ""
+        
+        // when the optional is nil, set a default value to 1
+        var numerator = ingredient.num ?? 1
+        var denominator = ingredient.denom ?? 1
+        var wholePortions = 0
+        
+        if ingredient.num != nil {
+            
+            // Get a single serving size by multiplying denominator by the recipe servinge
+            denominator *= recipeServings
+            
+            // Get target portion by multiplying numerator by target servings
+            numerator *= targetServings
+            
+            // Reduce fraction by greatest common divisor - greatest common divisor is handled in Rational.swift
+            let divisor = Rational.greatestCommonDivisor(numerator, denominator)
+            numerator /= divisor
+            denominator /= divisor
+            
+            // Get the whole portion if numerator > denominator
+            if numerator >= denominator {
+                
+                // Calculate the whole portions
+                wholePortions = numerator / denominator
+                
+                // Calculate the remainder
+                numerator = numerator % denominator
+                
+                // Assign to portion string
+                portion += String(wholePortions)
+            }
+            
+            
+            // Express the remainder as a fraction
+            if numerator > 0 {
+                
+                // Assign remainder as fraction to the portion string
+                portion += wholePortions > 0 ? " " : ""
+                portion += "\(numerator)/\(denominator)"
+            }
+        }
+        
+        if var unit = ingredient.unit {
+                        
+            // If units need to be plural
+            if wholePortions > 1 {
+                
+                // Calculate appropriate suffix
+                if unit.suffix(2) == "ch" {
+                    unit += "es"
+                }
+                else if unit.suffix(1) == "f" {
+                    unit = String(unit.dropLast())
+                    unit += "ves"
+                }
+                else {
+                    unit += "s"
+                }
+            }
+            
+            // Calculate appropriate unit
+            
+            
+            portion += ingredient.num == nil && ingredient.denom == nil ? "" : " "
+            
+            return portion + unit
+            
+            
+        }
+        
+        
+        return portion
+        
+    }
+    
     
 }
